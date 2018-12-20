6e498b6b3f0a208e32f670e0c39382305d5bf93f
losandluca <lewisalcazar88@gmail.com>
2018-12-20 16:49:32 -0700

    working on logic

---
+++ asssets/main.js
@@ -0,0 +1,28 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCiJRW1LSebHXvIxDYepo3YdvKvGyHpv3E",
+    authDomain: "train-scheduler-6dcc0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-6dcc0.firebaseio.com",
+    projectId: "train-scheduler-6dcc0",
+    storageBucket: "",
+    messagingSenderId: "125573925361"
+  };
+  firebase.initializeApp(config);
+
+  var trainDatabase = firebase.database();
+
+  $("#add-train-btn").on("click", function(){
+
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var trainTime = $("#train-time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
+    var newTrainAdded = {
+        name: trainName,
+        destination : destination,
+        trainTime: trainTime,
+        frequency: frequency
+    };
+
+  })
